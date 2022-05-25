cb6eb66acbcc754398916177dbbf94c2e84b7970
unknown <lizanchen@game.ntes>
2022-05-25 21:14:42 +0800

    add more commments in aibird file

--- src1/fifty-bird/bird12/AiBird.lua
+++ src1/fifty-bird/bird12/AiBird.lua
@@ -9,7 +9,9 @@ function AiBird:init()
 end
 
 function AiBird:update(dt, pair)
+    -- bird's uplimit - jump's height > bottom of the top pipe (y=0 is the top,not the bottom of the screen)
     if self.bird.y-3*v0*v0/(2*GRAVITY) > pair.y+PIPE_HEIGHT then
+        -- bird's top position + bird height + bottom detection box grace distance > top of the bottom pipe)
         if self.bird.y+self.bird.height+15 > pair.y+PIPE_HEIGHT+GAP_HEIGHT then
          love.keypressed('space')
         end
